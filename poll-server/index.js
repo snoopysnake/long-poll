0bcd96d90b8df5705e7ff64725731c4188970206
snoopysnake <chiang4@gmail.com>
2021-03-24 16:42:38 -0400

    Created answer endpoint

--- poll-server/index.js
+++ poll-server/index.js
@@ -1,9 +1,20 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 8000
 
+app.use(cors())
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/sendAnswer', (req, res) => {
+  const num = req.body.num;
+  if (!isNaN(num) && num > 0 && num <= 4)
+    res.send(`Received answer ${req.body.num}!`)
+  else res.send('Not a valid number')
 })
 
 app.listen(port, () => {
